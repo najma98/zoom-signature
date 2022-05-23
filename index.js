fa78ef853c6c2a1450def46ddc1a0b5e39da72b1
Mariinja <marinja.principe@uzh.ch>
2022-05-23 11:48:03 +0200

    add body parse

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const KJUR = require('jsrsasign')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(cors({
+app.use(bodyParser.json(), cors({
     origin: ["https://sopra22-group11-thegame-client.herokuapp.com", "http://localhost", "http://localhost:3000"]
 }))
 
